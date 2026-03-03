91c2eed43b6b07c63eca3f947f70466565c55a60
Oliver Strauss <Ols756@iastate.edu>
2026-03-02 18:33:58 -0600

    fixes stuff

--- ChessEngine/ChessEngine/ChessEngine.Backend/obj/Debug/net8.0/ChessEngine.Backend.AssemblyInfo.cs
+++ ChessEngine/ChessEngine/ChessEngine.Backend/obj/Debug/net8.0/ChessEngine.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessEngine.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+912ab7d249befd366dc33e6273df60790b8dcbf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a656630e8f7aa2734060bcdc7c6d855422f37120")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessEngine.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessEngine.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
